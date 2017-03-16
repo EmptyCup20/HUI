e200a430912dd2b4a2d004cfb8810265a5318ce4
张鑫14 <zhangxin14@hikvision.com.cn>
2017-03-16 14:12:06 +0800

    加入跨域请求处理

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, accept, origin, content-type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    //res.header("Access-Control-Max-Age: 86400");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.use('/', require('./server/routes/front/index.router.js'));
 app.use('/index', require('./server/routes/front/index.router.js'));
 app.use('/design', require('./server/routes/front/design.router.js'));
